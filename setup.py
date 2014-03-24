4211ecb1bddadd8575392acc7721c8365f4bfb5b
Thomas Amland <thomas.amland@gmail.com>
2014-03-24 12:41:36 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name = 'parsedom',
+      version = '1.0.0',
+      author = 'Thomas Amland',
+      author_email = 'thomas.amland@googlemail.com',
+      license = 'GPLv3',
+      description = 'A fast DOM parser',
+      long_description = open('README.rst').read(),
+      packages = ['parsedom'],
+      classifiers = ["Development Status :: 5 - Production/Stable",
+                     "Programming Language :: Python",
+                     "Operating System :: OS Independent",
+                     "Topic :: Text Processing :: Markup :: HTML",
+                     "Topic :: Text Processing :: Markup :: XML",
+                     "Topic :: Software Development :: Libraries :: Python Modules"],
+)
